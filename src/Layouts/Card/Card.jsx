532b18c9e163ec525424f4463a046d032ed9e342
Sena <dimatesebas73@gmail.com>
2025-04-24 12:55:06 -0500

    feat: Imprimir productos

--- src/Layouts/Card/Card.jsx
+++ src/Layouts/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export const Card = () => {
-
+export const Card = (products) => {
+const {title,price,description,category,image} = products
     return (
     
-    <div className=''>
-        <h2 className=''></h2>
-        <h3 className=''></h3>
-        <p className=''></p>
-        <h3 className=''></h3>
-        <img src="" alt="" className='rounded-lg'/>    
+    <div className='bg-white shadow-md rounded-lg p-4 flex flex-col items-center w-[30rem] align-center gap-3'>
+        <h2 className='text-[1.5rem]'>{title}</h2>
+        <h3 className='text-[0.8rem]'>{price}</h3>
+        <p className='text-[0.8rem]'>{description}</p>
+        <h3 className=''>{category}</h3>
+        <img src={image} alt="" className='rounded-lg h-[10rem]'/>    
 
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,27 @@
 import { useEffect, useState } from "react"
-
+import { Card } from "../../Layouts/Card/Card"
+const URL = 'https://fakestoreapi.com/products'
 
 export const Home = () => {
 
+  const [products, setProducts] = useState([])
+
+  const handleGetProducts = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setProducts(data)
+  }
+
+  return (
+    <>
+      <button className="" onClick={handleGetProducts}>Traer Productos</button>
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        {
+          products.map(products => (
+            <Card key={products.id} {...products} />
+          ))
+        }
+      </section>
+    </>
+  )
 }
