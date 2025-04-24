ff080a10318381347f322ff19f2effdbf461dcdf
Sena <dimatesebas73@gmail.com>
2025-04-24 12:40:43 -0500

    Feat: maquetacion

--- src/Layouts/Card/Card.jsx
+++ src/Layouts/Card/Card.jsx
@@ -5,7 +5,11 @@ export const Card = () => {
     return (
     
     <div className=''>
-     
+        <h2 className=''></h2>
+        <h3 className=''></h3>
+        <p className=''></p>
+        <h3 className=''></h3>
+        <img src="" alt="" className='rounded-lg'/>    
 
     </div>
   )
--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,10 +1,23 @@
-
+import { NavBar } from '../NavBar/NavBar';
+import { ItemTitles } from '../../components/ItemTitles/ItemTitles';
+import { ItemNavBar } from '../../components/itemNavBar/ItemNavBar';
 
 export const Header = () => {
 
   return (
-    <header className="">
-      
+    <header className="sticky top-0 left-0 z-50 bg-blue-700 px-6 py-5 shadow-xl w-full">
+      <div className="flex items-center flex-col gap-5">
+   
+        <ItemTitles content="Consumo de Api's" styles="text-2xl sm:text-4xl text-white font-bold" />
+
+        
+        <NavBar>
+          <ul className='flex align-center space-x-15'>
+            <ItemNavBar route="/" content="HOME" styles="text-white text-lg" />
+            
+          </ul>
+        </NavBar>
+      </div>
     </header>
   );
 };
